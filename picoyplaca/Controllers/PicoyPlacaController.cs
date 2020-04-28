9524e10007823a9bd81b32790c3222b737a250b0
Luis <antjazl@gmail.com>
2020-04-28 13:53:12 -0500

    first commit

---
+++ picoyplaca/Controllers/PicoyPlacaController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace picoyplaca.Controllers
+{
+    public class PicoyPlacaController : Controller
+    {
+        // GET: PicoyPlaca
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Resultado(string date, string lastDigit, string time, string day )
+        {
+            string[] timeDetail= time.Split(':');
+            TimeSpan input = new TimeSpan(Int32.Parse(timeDetail[0]), Int32.Parse(timeDetail[1]), 0);
+            TimeSpan start = new TimeSpan(7, 0, 0); //7 o'clock
+            TimeSpan end = new TimeSpan(9, 30, 0); //9:30 o'clock
+            TimeSpan start2 = new TimeSpan(16,0,0);//16:00
+            TimeSpan end2 = new TimeSpan(19,30,0);//19:30
+           string html = "";
+            
+            switch (day) {
+                case "1":  //Lunes
+                    if (!lastDigit.Equals("1") && !lastDigit.Equals("2")) { html += "Su auto puede circular"; }
+                    if ((lastDigit.Equals("1") || lastDigit.Equals("2")) && (input < start || input > end)&& (input < start2 || input > end2)) { html += "Su auto puede circular"; }
+                    if ((lastDigit.Equals("1") || lastDigit.Equals("2")) && ((input > start && input < end)|| (input > start2 && input < end2))) { html += "Su auto NO puede circular"; }
+                    break;
+                case "2": // Martes
+                    if (!lastDigit.Equals("3") && !lastDigit.Equals("4")) { html += "Su auto puede circular"; }
+                    if ((lastDigit.Equals("3") || lastDigit.Equals("4")) && (input < start || input > end) && (input < start2 || input > end2)) { html += "Su auto puede circular"; }
+                    if ((lastDigit.Equals("3") || lastDigit.Equals("4")) && ((input > start && input < end) || (input > start2 && input < end2))) { html += "Su auto NO puede circular"; }
+                    break;
+                case "3":// Miercoles
+                    if (!lastDigit.Equals("5") && !lastDigit.Equals("6")) { html += "Su auto puede circular"; }
+                    if ((lastDigit.Equals("5") || lastDigit.Equals("6")) && (input < start || input > end) && (input < start2 || input > end2)) { html += "Su auto puede circular"; }
+                    if ((lastDigit.Equals("5") || lastDigit.Equals("6")) && ((input > start && input < end) || (input > start2 && input < end2))) { html += "Su auto NO puede circular"; }
+                    break;
+                case "4"://Jueves
+                    if (!lastDigit.Equals("7") && !lastDigit.Equals("8")) { html += "Su auto puede circular"; }
+                    if ((lastDigit.Equals("7") || lastDigit.Equals("8")) && (input < start || input > end) && (input < start2 || input > end2)) { html += "Su auto puede circular"; }
+                    if ((lastDigit.Equals("7") || lastDigit.Equals("8")) && ((input > start && input < end) || (input > start2 && input < end2))) { html += "Su auto NO puede circular"; }
+                    break;
+                case "5"://Viernes
+                    if (!lastDigit.Equals("9") && !lastDigit.Equals("0")) { html += "Su auto puede circular"; }
+                    if ((lastDigit.Equals("9") || lastDigit.Equals("0")) && (input < start || input > end) && (input < start2 || input > end2)) { html += "Su auto puede circular"; }
+                    if ((lastDigit.Equals("9") || lastDigit.Equals("0")) && ((input > start && input < end) || (input > start2 && input < end2))) { html += "Su auto NO puede circular"; }
+                    break;
+
+                default:
+                    html += "Su auto puede circular";
+                    break;
+                 
+
+            }
+
+
+
+            return Json(new { html }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ picoyplaca/Scripts/Validar.js
@@ -0,0 +1,35 @@
+﻿var btnValidate = document.getElementById("btnValidate");
+
+
+
+btnValidate.onclick = function () {
+
+    var plate = document.getElementById("txtplate").value;//PLACA
+    var lastDigitPlate = plate[plate.length - 1];     //ULTIMO DIGITO DE LA PLACA
+    var inputdate = document.getElementById("date-input").value;
+    var hour = document.getElementById("hour").value;
+    var date = new Date($('#date-input').val());
+    day = date.getUTCDay();
+    var html = "";
+    $.ajax({
+        url: '/PicoyPlaca/Resultado',
+        type: "GET",
+        dataType: "json",
+        data: { date: inputdate, lastDigit: lastDigitPlate, time: hour, day: day},
+        error: function (response) {
+            alert("error");
+        },
+        success: function (response) {
+            html = response.html;
+            alert(html);
+        }
+    });
+}
+
+var btnLimpiar = document.getElementById("btnLimpiar");
+btnLimpiar.onclick = function () {
+    document.getElementById("txtplate").value = "";
+    document.getElementById("txtdate").value = "";
+    document.getElementById("lblRespuesta").innerHTML = "";
+
+}
